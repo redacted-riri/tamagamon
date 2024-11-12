d177eaae692bf1faa99b61e772a3bc6f88ecdea9
redacted-riri <sariah.echols.18@gmail.com>
2024-11-11 23:28:08 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # tamagamon
 Tamagotchi/Pokemon personalized (don't sue me Nintendo)
+
+A fun (hopefully short) project to create a desktop widget that will allow you to take care of a little pet. Mostly a project for fun to improve my programming and artistic skills. 
---
+++ main.c
@@ -0,0 +1,44 @@
+#include <SDL2/SDL.h>
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+    // Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    // Create a window
+    SDL_Window* window = SDL_CreateWindow("SDL Project",
+                                          SDL_WINDOWPOS_CENTERED,
+                                          SDL_WINDOWPOS_CENTERED,
+                                          640, 480,
+                                          SDL_WINDOW_SHOWN);
+    if (window == NULL) {
+        printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    // Main loop flag
+    int quit = 0;
+    SDL_Event e;
+
+    // Event loop
+    while (!quit) {
+        while (SDL_PollEvent(&e) != 0) {
+            // User requests quit
+            if (e.type == SDL_QUIT) {
+                quit = 1;
+            }
+        }
+
+        // Add rendering code here
+    }
+
+    // Clean up and close SDL
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
